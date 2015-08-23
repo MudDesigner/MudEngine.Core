8ad17bcff4ab2a58168d46dce594ac9fefd364cb
Johnathon Sullinger <Johnathon@sullinger.us>
2015-08-23 16:44:23 -0700

    Converted the MudEngine.Networking project into a PCL.

--- Src/MudEngine.Networking/ServerStartupContext.cs
+++ Src/MudEngine.Networking/ServerStartupContext.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MudDesigner.MudEngine.Networking
+﻿namespace MudDesigner.MudEngine.Networking
 {
     public interface IServerContext
     {
