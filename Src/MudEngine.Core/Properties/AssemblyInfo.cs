2b8f9471d76b69b3bfda6c8ec8eeebff53d64fdd
Johnathon Sullinger <Johnathon@sullinger.us>
2016-01-23 18:58:57 -0700

    Fixed assembly information

--- Src/MudEngine.Core/Properties/AssemblyInfo.cs
+++ Src/MudEngine.Core/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mud Designer engine core")]
+[assembly: AssemblyTitle("MudDesigner.MudEngine.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Source")]
+[assembly: AssemblyProduct("MudDesigner.MudEngine.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Src/MudEngine.Networking/Properties/AssemblyInfo.cs
+++ Src/MudEngine.Networking/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MudEngine.Networking")]
+[assembly: AssemblyTitle("MudDesigner.MudEngine.Networking")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MudEngine.Networking")]
+[assembly: AssemblyProduct("MudDesigner.MudEngine.Networking")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
