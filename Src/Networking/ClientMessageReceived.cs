fe9cff2b444983fcaa0447dce521f196d0c904f8
Johnathon Sullinger <johnathon@sullinger.us>
2016-05-22 19:16:18 -0700

    Migrated networking APIs into the Core project.

---
+++ Src/Networking/ClientMessageReceived.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ClientMessageReceived.cs" company="Sully">
+//     Copyright (c) Johnathon Sullinger. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace MudDesigner.MudEngine.Networking
+{
+    /// <summary>
+    /// MessageBroker message type for when the server receives data from the client
+    /// </summary>
+    public class ClientMessageReceived : MessageBase<ClientData>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientMessageReceived"/> class.
+        /// </summary>
+        /// <param name="data">The data sent by the client.</param>
+        public ClientMessageReceived(ClientData data)
+        {
+            this.Content = data;
+        }
+    }
+}
