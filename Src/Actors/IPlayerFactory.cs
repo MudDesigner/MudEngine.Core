2090f45631902e0c786b8b211448e929897e6caa
Johnathon Sullinger <johnathon@sullinger.us>
2016-05-24 21:44:01 -0700

    Removal of Actors, Environments, Commanding and Networking namespaces. Those are better served in their own independent adapter packages

--- Src/Actors/IPlayerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using MudDesigner.MudEngine.Commanding;
-
-namespace MudDesigner.MudEngine.Actors
-{
-    public interface IPlayerFactory
-    {
-        IPlayer CreatePlayer(IActorCommand initialCommand);
-    }
-}
